3fced195588dd8cf0c781a5b377bb4f9954180b1
hungvo2003vn <votanhung2003@gmail.com>
2024-11-01 02:41:52 +0700

    +nodejs add more distributions

--- producer-service/config.js
+++ producer-service/config.js
@@ -36,12 +36,18 @@ const BATCH_SIZE = {
     MEAN: 30,
     STD_DEV: 15,
     MIN: 1,
-    MAX: 70
+    MAX: 70,
+    MEAN1: 32,
+    NUM_MODES: 3,
+    LAMBDA: 0.05
 }
 const DISTRIBUTIONS = {
     BELL_CURVE: 'Bell Curve',
     UNIFORM: 'Uniform',
-    LINEAR: 'Linear'
+    LINEAR: 'Linear',
+    BIMODAL: 'Bimodal',
+    MULTIMODAL: 'Multimodal',
+    EXPONENTIAL: 'Exponential'
 }
 const RANDOM_ORDER = false
 
--- producer-service/helpers/distribution.js
+++ producer-service/helpers/distribution.js
@@ -1,4 +1,4 @@
-const { DISTRIBUTIONS } = require('../config')
+const { DISTRIBUTIONS, BATCH_SIZE } = require('../config')
 
 const generateBatchSizes = (totalSlots, mean, stdDev, min, max, distribution, randomOrder) => {
     let batchSizes = []
@@ -13,6 +13,15 @@ const generateBatchSizes = (totalSlots, mean, stdDev, min, max, distribution, ra
             break
         case DISTRIBUTIONS.LINEAR:
             batchSizes = linearBatchSizes(totalSlots, min, max)
+            break
+        case DISTRIBUTIONS.BIMODAL:
+            batchSizes = bimodalBatchSizes(totalSlots, mean, BATCH_SIZE.MEAN1, stdDev, min, max)
+            break
+        case DISTRIBUTIONS.MULTIMODAL:
+            batchSizes = multimodalBatchSizes(totalSlots, mean, stdDev, min, max, BATCH_SIZE.NUM_MODES)
+            break
+        case DISTRIBUTIONS.EXPONENTIAL:
+            batchSizes = exponentialBatchSizes(totalSlots, min, max, BATCH_SIZE.LAMBDA)
             break
         default:
             throw new Error(`Unsupported distribution type ${distribution}`)
@@ -58,6 +67,58 @@ const linearBatchSizes = (totalSlots, min, max) => {
 
     for (let i = 0; i < totalSlots; i++) {
         const batchSize = Math.round(min + i * step)
+        batchSizes.push(batchSize)
+    }
+
+    return batchSizes
+}
+
+const bimodalBatchSizes = (totalSlots, mean1, mean2, stdDev, min, max) => {
+    const batchSizes = []
+    const midPoint1 = Math.floor(totalSlots / 3)
+    const midPoint2 = Math.floor((2 * totalSlots) / 3)
+    const variance = stdDev * stdDev
+
+    for (let i = 0; i < totalSlots; i++) {
+        const x1 = i - midPoint1
+        const x2 = i - midPoint2
+        const peak1 = Math.exp(-x1 * x1 / (2 * variance)) * mean1
+        const peak2 = Math.exp(-x2 * x2 / (2 * variance)) * mean2
+
+        let biModalValue = (peak1 + peak2) / 2
+        let batchSize = Math.round(biModalValue * max)
+        batchSize = Math.max(min, batchSize)
+
+        batchSizes.push(batchSize)
+    }
+
+    return batchSizes
+}
+
+
+const multimodalBatchSizes = (totalSlots, mean, stdDev, min, max, numModes) => {
+    const batchSizes = []
+    const interval = Math.floor(totalSlots / numModes)
+    const variance = stdDev * stdDev
+
+    for (let i = 0; i < totalSlots; i++) {
+        const modeCenter = Math.floor((i % interval) * (totalSlots / interval))
+        const x = i - modeCenter
+        const modalValue = Math.exp(-x * x / (2 * variance)) * mean
+        let batchSize = Math.round(modalValue * max)
+        batchSize = Math.max(min, batchSize)
+        batchSizes.push(batchSize)
+    }
+
+    return batchSizes
+}
+
+const exponentialBatchSizes = (totalSlots, min, max, lambda) => {
+    const batchSizes = []
+    for (let i = 0; i < totalSlots; i++) {
+        const expValue = Math.exp(-lambda * i)
+        let batchSize = Math.round(expValue * max)
+        batchSize = Math.max(min, Math.min(batchSize, max))
         batchSizes.push(batchSize)
     }
 
